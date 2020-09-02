30b2f20ed4dc21d52a072d747a3ffd0e36e804e2
developer <developer@plouto.finance>
2020-09-02 19:39:23 +0800

    delete test network

--- migrations/5_deploy_strategy_dai_curve.js
+++ migrations/5_deploy_strategy_dai_curve.js
@@ -1,22 +1,9 @@
 const Controller = artifacts.require('Controller');
 const StrategyDAICurve = artifacts.require('StrategyDAICurve');
 
-module.exports = async function(deployer, network) {
+module.exports = async function(deployer) {
   await deployer;
   await deployer.deploy(StrategyDAICurve, Controller.address);
-  if (network.includes('ropsten')) {
-    const contractInstance = await deployer.deploy(Controller, { overwrite: false });
-    await contractInstance.setStrategy(
-      '0xADeD4B66783099E174a17b74E698aeFA0fd8f19d',
-      StrategyDAICurve.address);
-    return;
-  } else if (network.includes('kovan')) {
-    const contractInstance = await deployer.deploy(Controller, { overwrite: false });
-    await contractInstance.setStrategy(
-      '0x5075a70f5c86a4132e57fcea857c0c1d87e43093',
-      StrategyDAICurve.address);
-    return;
-  }
   const contractInstance = await deployer.deploy(Controller, { overwrite: false });
   await contractInstance.setStrategy(
     '0x6b175474e89094c44da98b954eedeac495271d0f',
