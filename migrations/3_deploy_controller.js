fd6382ce1eecc8049a6bdd4c9b18159ad2c979fa
developer <developer@plouto.finance>
2020-09-24 11:25:11 +0800

    reset for online

--- migrations/3_deploy_controller.js
+++ migrations/3_deploy_controller.js
@@ -3,5 +3,5 @@ const Controller = artifacts.require('Controller');
 module.exports = async function(deployer) {
   await deployer;
   const contractInstance = await deployer.deploy(Controller);
-  await contractInstance.setRewards('0x2E44E2F1E79077210914B6D948B1aD0b0cd2b1EC');
+  await contractInstance.setRewards('0x025c1534f82996437Ad862FED39F02B973969362');
 };
